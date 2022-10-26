0f33b279d250f6de04d5089847121bf01ce70d9f
S4dBoi <levietbachvt02@gmail.com>
2022-10-26 21:25:02 +0700

    move index folder

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const express = require("express");
-const router = express.Router();
-var shajs = require('sha.js')
-router.post('/', (req, res, next) => {
-    const data = req.body;
-    console.log(req.body);
-    if (!data) {
-        res.status(400).send({ status: 'failed' });
-        return;
-    }
-    if (data.username === '' || data.password === '') {
-        res.status(400).send({ status: 'failed' });
-        return;
-    }
-    else {
-        data.password = shajs('sha256').update(data.password).digest('hex')
-        res.status(200).send(data);
-    }
+const express = require('express');
+const port = 3001;
+const router = require("./routes");
+const cors = require("cors");
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+var bodyParser = require('body-parser')
+var app = express()
 
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-module.exports = router;
+// parse application/json
+app.use(bodyParser.json())
+app.use(cors(corsOptions))
+
+app.get('/', (req, res) => res.status(200).send('Hi!'));
+app.post('/', router);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

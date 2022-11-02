39a9152ddf32b2b8e38070ee5544a283fc051c99
S4dBoi <levietbachvt02@gmail.com>
2022-11-02 15:36:37 +0700

    fix vercel config

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
-var indexRouter = require('../routes/index');
-var usersRouter = require('../routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 const cors = require("cors");
 const corsOptions = {
   origin: '*',

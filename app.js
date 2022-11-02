ed492c080a956091c9d3c47274650416c76b7f2d
S4dBoi <levietbachvt02@gmail.com>
2022-11-02 14:12:17 +0700

    add vercel config

--- app.js
+++ app.js
@@ -21,9 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
 
 app.use(logger('dev'));
 app.use(express.json());
